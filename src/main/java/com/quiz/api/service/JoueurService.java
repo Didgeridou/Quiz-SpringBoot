51863f4e87ff526876e8d7bf8695fb0db524b5a5
Didgeridou <UT@UT-PC.localdomain>
2026-05-25 00:11:32 +1100

    JoueurService updated

--- src/main/java/com/quiz/api/service/JoueurService.java
+++ src/main/java/com/quiz/api/service/JoueurService.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.quiz.api.entity.Joueur;
 import com.quiz.api.repository.JoueurRepository;
