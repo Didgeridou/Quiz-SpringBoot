da7fd8550829fa4b44d4e665cd7861ace82c10b7
Didgeridou <UT@UT-PC.localdomain>
2026-05-25 00:09:26 +1100

    QuizRepository updated

--- src/main/java/com/quiz/api/repository/QuizRepository.java
+++ src/main/java/com/quiz/api/repository/QuizRepository.java
@@ -1,10 +1,7 @@
 package com.quiz.api.repository;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.quiz.api.dto.QuizRequest;
 import com.quiz.api.entity.Quiz;
 
 public interface QuizRepository extends JpaRepository<Quiz, Long> { 
